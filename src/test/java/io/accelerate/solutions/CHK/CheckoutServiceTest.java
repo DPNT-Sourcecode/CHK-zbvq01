ed632076c2d677c1f6580449cd5f7527100f0110
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 17:06:05 +0000

    Wed Mar 04 17:06:05 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
@@ -18,29 +18,29 @@ public class CheckoutServiceTest {
     }
 
 
-     @Test
-     public void testCalculateTotal() {
-         Product productA = new ProductImpl("A", 50, 130, 3);
-         Product productB = new ProductImpl("B", 30, 45, 2);
-         Product productC = new ProductImpl("C", 20, null, null);
-         Product productD = new ProductImpl("D", 15, null, null);
+    @Test
+    public void testCalculateTotal() {
+        Product productA = new ProductImpl("A", 50, 130, 3);
+        Product productB = new ProductImpl("B", 30, 45, 2);
+        Product productC = new ProductImpl("C", 20, null, null);
+        Product productD = new ProductImpl("D", 15, null, null);
 
-         List<Product> products = Arrays.asList(productA, productA, productA, productB, productB, productC, productD);
-         Integer totalPrice = checkoutService.calculateTotal(products);
-         System.out.println("Total Price: " + totalPrice); // Expected output: Total Price: 195
-     }
+        List<Product> products = Arrays.asList(productA, productA, productA, productB, productB, productC, productD);
+        Integer totalPrice = checkoutService.calculateTotal(products);
+        System.out.println("Total Price: " + totalPrice); // Expected output: Total Price: 195
+    }
 
-     @Test
+    @Test
     public void testSimpleProductA() {
         Product productA = new ProductImpl("A", 50, 130, 3);
         Product productA2 = new ProductImpl("A", 50, 130, 3);
         List<Product> products = Arrays.asList(productA, productA2);
         Integer totalPrice = checkoutService.calculateTotal(products);
         System.out.println("Total Price: " + totalPrice); // Expected output: Total Price: 100
-         assertThat(totalPrice, equalTo(100));
-     }
+        assertThat(totalPrice, equalTo(100));
+    }
 
-     @Test
+    @Test
     public void testCombinationOfProducts() {
         Product productA = new ProductImpl("A", 50, 130, 3);
         Product productB = new ProductImpl("B", 30, 45, 2);
@@ -48,8 +48,23 @@ public class CheckoutServiceTest {
         List<Product> products = Arrays.asList(productA, productB, productC);
         Integer totalPrice = checkoutService.calculateTotal(products);
         System.out.println("Total Price: " + totalPrice); // Expected output: Total Price: 100
-         assertThat(totalPrice, equalTo(100));
-     }
+        assertThat(totalPrice, equalTo(100));
+    }
+
+
+    @Test
+    public void testMultipleEqualsProducts() {
+        Product productA = new ProductImpl("A", 50, 130, 3);
+        Product productA2 = new ProductImpl("A", 50, 130, 3);
+        Product productA3 = new ProductImpl("A", 50, 130, 3);
+
+        List<Product> products = Arrays.asList(productA, productA2, productA3);
+        Integer totalPrice = checkoutService.calculateTotal(products);
+        System.out.println("Total Price: " + totalPrice); // Expected output: Total Price: 130
+        assertThat(totalPrice, equalTo(130));
+    }
+
 }
+
 
 
