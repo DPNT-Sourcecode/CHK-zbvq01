ae794c4df350a0fd97a5598a4eda05f55de9a0ef
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 22:23:59 +0000

    Wed Mar 04 22:23:59 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutController.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutController.java
@@ -11,9 +11,12 @@ public class CheckoutController{
 
     private final CheckoutService checkoutService;
 
+    private final PricingRuleAggregator pricingRuleAggregator;
+
     public CheckoutController() {
         Map<String, Product> catalog = initializeCatalog();
-        this.checkoutService = new CheckoutService(new SKUParser(), catalog, initializePricingRules(catalog));
+        this.pricingRuleAggregator = new PricingRuleAggregator(catalog);
+        this.checkoutService = new CheckoutService(new SKUParser(), catalog, pricingRuleAggregator.getPricingRules());
     }
 
     public Integer checkout(String skus) {
@@ -51,124 +54,4 @@ public class CheckoutController{
 
         return catalog;
     }
-
-    private List<PricingRule> initializePricingRules(Map<String, Product> catalog) {
-        List<PricingRule> rules = new ArrayList<>();
-        //multi SKU RULE first
-        //E's multi-buy discount), price of 2E only
-        rules.add(new MultiSkuBundlePricingRule(
-                Map.of("E", 2, "B", 1),
-                2 * 40,
-                400));
-        //N's multi-buy discount), price of 3N only
-        rules.add(new MultiSkuBundlePricingRule(
-                Map.of("N", 3, "M", 1),
-                3 * 40,
-                400));
-        //R's multi-buy discount), price of 3R only
-        rules.add(new MultiSkuBundlePricingRule(
-                Map.of("R", 3, "Q", 1),
-                3 * 50,
-                400));
-
-
-
-
-
-        //single SKU bundle larger first
-        // A's multi-buy discount with higher priority
-        rules.add(new SimpleSkuBundlePricingRule(
-                "A", //SKU
-                5, //required qty
-                200, //price for 5 A's
-                300)); //priority
-
-        rules.add(new SimpleSkuBundlePricingRule(
-                "A", //SKU
-                3, //required qty
-                130, //price for 5 A's
-                200));
-
-        //H Offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "H", //SKU
-                10, //required qty
-                80, //price for 10 H's
-                300)); //priority
-
-        rules.add(new SimpleSkuBundlePricingRule(
-                "H", //SKU
-                5, //required qty
-                45, //price for 5 H's
-                200));
-
-        //V Offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "V", //SKU
-                3, //required qty
-                130, //price for 3 v's
-                300)); //priority
-        //V Offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "V", //SKU
-                2, //required qty
-                90, //price for 2 V's
-                200));
-
-        //B offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "B", //SKU
-                2, //required qty
-                45, //price for 4 B's
-                200)); //priority
-        //K offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "K", //SKU
-                2, //required qty
-                150, //price for 2 K's
-                200)); //priority
-        //P offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "P", //SKU
-                5, //required qty
-                200, //price for 5 Ps
-                200)); //priority
-        //Q offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "Q", //SKU
-                3, //required qty
-                80, //price for 3 Qs
-                200)); //priority
-
-
-
-
-
-
-        //F offer buy 2 get 1 free (need 3 F's to get the discount)
-        rules.add(new BuyXGetYFreeSameSkuPricingRule(
-                "F",
-                2,
-                1,
-                catalog.get("F").getUnitPrice(),
-                250));
-
-        //U offer buy 3 get 1 free (need 3 U's to get the discount)
-        rules.add(new BuyXGetYFreeSameSkuPricingRule(
-                "U",
-                3,
-                1,
-                catalog.get("U").getUnitPrice(),
-                250));
-
-
-
-        //fallback to unit price for any remaining items
-        rules.add(new UnitPricePricingRule(catalog));
-
-        return rules;
-    }
 }
-
-
-
--- src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
@@ -3,9 +3,7 @@ package io.accelerate.solutions.CHK;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,6 +14,8 @@ public class CheckoutServiceTest {
 
     @BeforeEach
     public void setUp() {
+
+
 
         Map<String, Product> catalog = new HashMap<>();
         catalog.put("A", new ProductImpl("A", 50));
@@ -44,53 +44,10 @@ public class CheckoutServiceTest {
         catalog.put("X", new ProductImpl("X", 90));
         catalog.put("Y", new ProductImpl("Y", 10));
         catalog.put("Z", new ProductImpl("Z", 50));
-        checkoutService = new CheckoutService(new SKUParser(), catalog, initializePricingRules(catalog));
+
+        PricingRuleAggregator pricingRuleAggregator = new PricingRuleAggregator(catalog);
+        checkoutService = new CheckoutService(new SKUParser(), catalog, pricingRuleAggregator.getPricingRules());
     }
-
-    private List<PricingRule> initializePricingRules(Map<String, Product> catalog) {
-        List<PricingRule> rules = new ArrayList<>();
-        //multi SKU RULE first
-        //E's multi-buy discount), price of 2E only
-        rules.add(new MultiSkuBundlePricingRule(
-                Map.of("E", 2, "B", 1),
-                2 * 40,
-                400));
-
-        //single SKU bundle larger first
-        // A's multi-buy discount with higher priority
-        rules.add(new SimpleSkuBundlePricingRule(
-                "A", //SKU
-                5, //required qty
-                200, //price for 5 A's
-                300)); //priority
-
-        rules.add(new SimpleSkuBundlePricingRule(
-                "A", //SKU
-                3, //required qty
-                130, //price for 5 A's
-                200));
-
-        //B offer
-        rules.add(new SimpleSkuBundlePricingRule(
-                "B", //SKU
-                2, //required qty
-                45, //price for 4 B's
-                200)); //priority
-
-        //F offer buy 2 get 1 free (need 3 F's to get the discount)
-        rules.add(new BuyXGetYFreeSameSkuPricingRule(
-                "F",
-                2,
-                1,
-                catalog.get("F").getUnitPrice(),
-                250));
-
-        //fallback to unit price for any remaining items
-        rules.add(new UnitPricePricingRule(catalog));
-
-        return rules;
-    }
-
 
 
     @Test
@@ -186,4 +143,3 @@ public class CheckoutServiceTest {
 
 
 }
-
