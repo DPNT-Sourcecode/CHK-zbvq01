805aef0f0277b2abfa828a4b04f448977cc2a7e0
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 17:00:05 +0000

    Wed Mar 04 17:00:05 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutService.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutService.java
@@ -10,22 +10,24 @@ public class CheckoutService {
     public Integer calculateTotal(List<Product> products) {
         Map<String, List> productMap = createProductMap(products);
 
-        //for each product in the map, calculate the total price based on the unit price and the discounted price
+        //for each product SKU in the map iterate over the list of those products, aggreate and the return the total price
         Integer totalPrice = 0;
         for (Map.Entry<String, List> entry : productMap.entrySet()) {
-            String sku = entry.getKey();
-            List<Product> productList = entry.getValue();
-            Product product = productList.get(0);
-            Integer unitPrice = product.getUnitPrice();
-            Integer discountedPrice = product.getDiscountedPrice();
-            Integer discountedMultiplier = product.getDiscountedMultiplier();
-            Integer quantity = productList.size();
-            if (discountedPrice != null && discountedMultiplier != null) {
-                Integer discountedQuantity = quantity / discountedMultiplier;
-                Integer remainingQuantity = quantity % discountedMultiplier;
-                totalPrice += discountedQuantity * discountedPrice + remainingQuantity * unitPrice;
-            } else {
-                totalPrice += quantity * unitPrice;
+            for (Object product : entry.getValue()) {
+                Product p = (Product) product;
+                if (p.getDiscountedPrice() != null && p.getDiscountedMultiplier() != null) {
+                    Integer discountedPrice = p.getDiscountedPrice();
+                    Integer discountedMultiplier = p.getDiscountedMultiplier();
+                    Integer unitPrice = p.getUnitPrice();
+                    Integer quantity = entry.getValue().size();
+
+                    //calculate the total price for the discounted products
+                    Integer discountedTotal = (quantity / discountedMultiplier) * discountedPrice;
+                    Integer remainingTotal = (quantity % discountedMultiplier) * unitPrice;
+                    totalPrice += discountedTotal + remainingTotal;
+                } else {
+                    totalPrice += p.getUnitPrice();
+                }
             }
         }
         return totalPrice;
@@ -46,5 +48,6 @@ public class CheckoutService {
 
 
 }
+
 
 
