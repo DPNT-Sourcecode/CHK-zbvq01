1d28a88c130984d51ab6854a67b70d30769d035c
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 20:38:53 +0000

    Wed Mar 04 20:38:53 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutServiceTest.java
@@ -3,8 +3,10 @@ package io.accelerate.solutions.CHK;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,40 +16,74 @@ public class CheckoutServiceTest {
 
     @BeforeEach
     public void setUp() {
-        checkoutService = new CheckoutService(new SKUParser(),);
+
+        Map<String, Product> catalog = new HashMap<>();
+        catalog.put("A", new ProductImpl("A", 50, 130, 3));
+        catalog.put("B", new ProductImpl("B", 30, 45, 2));
+        catalog.put("C", new ProductImpl("C", 20, null, null));
+        catalog.put("D", new ProductImpl("D", 15, null, null));
+        catalog.put("E", new ProductImpl("E", 40, null, null));
+
+
+
+        checkoutService = new CheckoutService(new SKUParser(), catalog, initializePricingRules(catalog));
     }
+
+    private List<PricingRule> initializePricingRules(Map<String, Product> catalog) {
+        List<PricingRule> rules = new ArrayList<>();
+        //multi SKU RULE first
+        //E's multi-buy discount), price of 2E only
+        rules.add(new MultiSkuBundlePricingRule(
+                Map.of("E", 2, "B", 1),
+                2 * 40,
+                400));
+
+        //single SKU bundle larger first
+        // A's multi-buy discount with higher priority
+        rules.add(new SimpleSkuBundlePricingRule(
+                "A", //SKU
+                5, //required qty
+                200, //price for 5 A's
+                300)); //priority
+
+        rules.add(new SimpleSkuBundlePricingRule(
+                "A", //SKU
+                3, //required qty
+                130, //price for 5 A's
+                200));
+
+        //B offer
+        rules.add(new SimpleSkuBundlePricingRule(
+                "B", //SKU
+                4, //required qty
+                60, //price for 4 B's
+                200)); //priority
+
+        //fallback to unit price for any remaining items
+        rules.add(new UnitPricePricingRule(catalog));
+
+        return rules;
+    }
+
 
 
     @Test
     public void testCalculateTotal() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productB = new ProductImpl("B", 30, 45, 2);
-        Product productC = new ProductImpl("C", 20, null, null);
-        Product productD = new ProductImpl("D", 15, null, null);
-
-        List<Product> products = Arrays.asList(productA, productB, productC, productD);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("ABCD");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(115));
     }
 
     @Test
     public void testSimpleProductA() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productA2 = new ProductImpl("A", 50, 130, 3);
-        List<Product> products = Arrays.asList(productA, productA2);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("AA");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(100));
     }
 
     @Test
     public void testCombinationOfProducts() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productB = new ProductImpl("B", 30, 45, 2);
-        Product productC = new ProductImpl("C", 20, null, null);
-        List<Product> products = Arrays.asList(productA, productB, productC);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("ABC");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(100));
     }
@@ -55,39 +91,21 @@ public class CheckoutServiceTest {
 
     @Test
     public void testMultipleEqualsProducts() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productA2 = new ProductImpl("A", 50, 130, 3);
-        Product productA3 = new ProductImpl("A", 50, 130, 3);
-
-        List<Product> products = Arrays.asList(productA, productA2, productA3);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("AAA");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(130));
     }
 
     @Test
     public void testMultipleEqualsProductsPlus1() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productA2 = new ProductImpl("A", 50, 130, 3);
-        Product productA3 = new ProductImpl("A", 50, 130, 3);
-        Product productA4 = new ProductImpl("A", 50, 130, 3);
-
-        List<Product> products = Arrays.asList(productA, productA2, productA3, productA4);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("AAAA");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(180));
     }
 
     @Test
     public void testTwoDifferentProductsWithDiscount() {
-        Product productA = new ProductImpl("A", 50, 130, 3);
-        Product productA2 = new ProductImpl("A", 50, 130, 3);
-        Product productA3 = new ProductImpl("A", 50, 130, 3);
-        Product productB = new ProductImpl("B", 30, 45, 2);
-        Product productB2 = new ProductImpl("B", 30, 45, 2);
-
-        List<Product> products = Arrays.asList(productA, productA2, productA3, productB, productB2);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("AAABB");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(175));
     }
@@ -95,11 +113,7 @@ public class CheckoutServiceTest {
 
     @Test
     public void testProducsWithNoDiscount() {
-        Product productC = new ProductImpl("C", 20, null, null);
-        Product productD = new ProductImpl("D", 15, null, null);
-
-        List<Product> products = Arrays.asList(productC, productD);
-        Integer totalPrice = checkoutService.calculateTotal(products);
+        Integer totalPrice = checkoutService.calculateTotal("CD");
         System.out.println("Total Price: " + totalPrice);
         assertThat(totalPrice, equalTo(35));
     }
@@ -108,3 +122,4 @@ public class CheckoutServiceTest {
 
 
 }
+
