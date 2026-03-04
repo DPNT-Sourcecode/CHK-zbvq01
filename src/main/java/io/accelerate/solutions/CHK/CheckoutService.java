8cf26a670896e579cd3db85294cdd2512459d77f
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 16:51:05 +0000

    Wed Mar 04 16:51:05 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutService.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutService.java
@@ -1,4 +1,42 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CheckoutService {
+    //given a list of products, calculate the total checkout price
+    public Integer calculateTotal(List<Product> products) {
+        Integer total = 0;
+        for (Product product : products) {
+            if (product.getUnitPrice() == -1) {
+                return -1;
+            }
+            if (product.getDiscountedPrice() != null && product.getDiscountedMultiplier() != null) {
+                Integer discountedTotal = (products.size() / product.getDiscountedMultiplier()) * product.getDiscountedPrice();
+                Integer remainingTotal = (products.size() % product.getDiscountedMultiplier()) * product.getUnitPrice();
+                total += discountedTotal + remainingTotal;
+            } else {
+                total += product.getUnitPrice();
+            }
+        }
+        return total;
+    }
+
+    //create a HashMap of products based on their SKU
+    //hashmap should contains the String sku and the list of products
+    public Map<String, List> createProductMap(List<Product> products) {
+        Map<String, List> productMap = new HashMap<>();
+        for (Product product : products) {
+            if (!productMap.containsKey(product.getSku())) {
+                productMap.put(product.getSku(), new ArrayList<>());
+            }
+            productMap.get(product.getSku()).add(product);
+        }
+        return productMap;
+    }
+
+
 }
+
--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,13 +1,45 @@
 package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         //checks if have any input
         if (StringUtils.isEmpty(skus)) {
             return 0;
+        } else {
+            //create a new instance for each product depending on the input
+            //create a list of products based on the list of chars in the input string
+            String[] skusArray = skus.split("");
+            List<Product> products = new ArrayList<>();
+            for (String sku : skusArray) {
+                switch (sku) {
+                    case "A":
+                        products.add(new ProductImpl("A", 50, 130, 3));
+                        break;
+                    case "B":
+                        products.add(new ProductImpl("B", 30, 45, 2));
+                        break;
+                    case "C":
+                        products.add(new ProductImpl("C", 20, null, null));
+                        break;
+                    case "D":
+                        products.add(new ProductImpl("D", 15, null, null));
+                        break;
+                    default:
+                        //for products that are not in the list, return value -1
+                       products.add(new ProductImpl("Z", -1, null, null));
+                }
+            }
+
+            //calculate the total price of the products in the list
+
         }
 
     }
 }
+
