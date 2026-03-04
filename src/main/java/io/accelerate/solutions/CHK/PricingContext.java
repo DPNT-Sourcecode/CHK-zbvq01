7efbdf8fa8f0b7f73a3f86e25a6f3e9259710bb7
sbx_user1051 <sbx_user1051@169.254.2.213>
2026-03-04 19:29:53 +0000

    Wed Mar 04 19:29:53 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/PricingContext.java
+++ src/main/java/io/accelerate/solutions/CHK/PricingContext.java
@@ -1,4 +1,27 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.Map;
+
 public class PricingContext {
+
+
+    private final Map<String, Integer> quantities;
+
+    public PricingContext(Map<String, Integer> quantities) {
+        this.quantities = quantities;
+    }
+
+    public int getQuantity(String productCode) {
+        return quantities.getOrDefault(productCode, 0);
+    }
+
+    public boolean contains(String sku) {
+        return quantities.containsKey(sku);
+    }
+    
+    public void consume(String sku, int amount) {
+        int current = quantities.get(sku);
+        quantities.put(sku, current - amount);
+    }
 }
+
